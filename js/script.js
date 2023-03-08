5616ce2064536dd00d486de114f730cabc8c683c
Mateus Volkart <volkartmateus@gmail.com.br>
2023-03-08 16:58:04 -0300

    part 2

--- js/script.js
+++ js/script.js
@@ -1,38 +1,47 @@
-let ator = new Ator(1, "JOHN WAYNE");
-console.log(ator)
+let inputBuscarFilme = document.querySelector("#input-buscar-filme");
+let btnBuscarFilme = document.querySelector("#btn-buscar-filme");
 
-let direcao = [
-    new Diretor(1, "lana wachowski"),
-    new Diretor(2, "Lilly wachowski")
-];
 
-let elenco = [
-    new Ator(1, "keanu reeves"),
-    new Ator(2, "Carrie-anne moss"),
-    new Ator(3, "laurence fishburne"),
-    new Ator(4, "joe pantoliano"),
-    new Ator(5, "hugo weaving"),
-    new Ator(6, "Antony ray parker"),
-]
+let listarFilmes = async(filmes) => {
+    let listaFilmes = await document.querySelector("lista-filmes");
+    listaFilmes.innerHTML = "";
+    console.log(listaFilmes);
+    if(filmes.length > 0) {
+        filmes.forEach(async(filme) => {
+            listaFilmes.appendChild(await filme.getCard());
+        });
+    }
+}
 
-let genero = "terror"
 
-let sinopse = "um jovem programador (keanu reeves) é atormentado por estranhos pesadelos nos quais sempre está com... "
+btnBuscarFilme.onclick = async () => {
+    if(inputBuscarFilme.value.length > 0){
+        let filmes = new Array();
+        fetch("http://www.omdbapi.com/?apikey=ba316868&s="+inputBuscarFilme.value)
+        .then((resp) => resp.json())
+        .then((resp)=> {
+        console.log(resp);
+        resp.Search.forEach((item)=>{
+            console.log(item);
+            let filme=new Filme(
+                item.imdbID,
+                item.Title,
+                item.Year,
+                null,
+                null,
+                item.poster,
+                null,
+                null,
+                null,
+                null,
+                null,
+                );
+                filmes.push(filme);
+            });
+            listarFilmes(filmes);
+        })
 
-let cartaz = "https://m.media-amazon.com/images/I/81LVfPpDHRS.jpg"
+    }
+    return false;
+}
 
-let filme = new Filme(
-    1,
-    "matrix",
-    1999,
-    genero,
-    102,
-    sinopse,
-    cartaz,
-    direcao,
-    elenco,
-    14,
-    null
-    );
-
-    console.log(filme)
